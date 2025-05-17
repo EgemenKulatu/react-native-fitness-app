31d77e4f4676479289c4640e5ba41d6518a723b2
EgemenKulatu <105080055+EgemenKulatu@users.noreply.github.com>
2025-05-17 22:39:08 +0300

    Fırst commit: Fitness app starter

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { useRouter } from 'expo-router';
+import { Button, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  const router = useRouter();
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 24, marginBottom: 20 }}>Hoş Geldin!</Text>
+      <Button title="Başla" onPress={() => router.push('/workout-type')} />
+    </View>
+  );
+}
---
+++ app/workout-type.tsx
@@ -0,0 +1,7 @@
+import { Text, View } from 'react-native';
+
+export default function WorkoutTypeScreen() {
+return (<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Nasıl bir antrenman yapmak istiyorsunuz?</Text>
+    </View>);
+}
